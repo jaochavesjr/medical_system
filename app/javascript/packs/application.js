689f71b9b3930cbd765a3c0be407d4af50be6df4
Joao Chaves <chavesjj@outlook.com>
2020-11-14 11:43:45 -0300

    config layout sbadmin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("jquery")
+require("jquery.easing/jquery.easing")
+require("bootstrap/dist/js/bootstrap.bundle")
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("src/sb-admin-2.min")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'dashboard#index'
+  #get 'dashboard', to: 'dashboard#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
